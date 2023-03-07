28d1cbc25380f1e8937b97b986009266f0d06642
cole-fiedler <123602079+cole-fiedler@users.noreply.github.com>
2023-03-07 18:59:29 +0000

    Test case gcd function

--- test/question_test_1/question_tests_1.cpp
+++ test/question_test_1/question_tests_1.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "question1.h"
+#include "question1.h" // includes function prototypes into test case
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -9,4 +9,9 @@ TEST_CASE("Verify Test Configuration", "verification") {
 TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
+}
+
+TEST_CASE("Verify find_gcd returns accurate result") // test gcd function to ensure correct result returned
+{
+	REQUIRE(find_gcd(15,25) == 5);
 }
