26979d2f83018d75fae2fdafe992e11c919fa525
cole-fiedler <123602079+cole-fiedler@users.noreply.github.com>
2023-03-08 19:02:16 +0000

    question 3 header, test case, cpp, main

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -1,7 +1,7 @@
-#include "question3.h"
-#include <iostream>
+#include "question3.h" // include header with function prototypes
+#include <iostream> // include input output stream
 
-using std::cout; using std::cin;
+using std::cout; using std::cin; // specify where to find cin and cout
 
 bool test_config()
 {
@@ -10,10 +10,28 @@ bool test_config()
 
 int roll_die() // roll die function uses no parameters and returns an int value
 {
+    int roll;
+    int min = 1; // the min number a die can roll is 1
+    int max = 6;// the max value is the die size of 6
 
+    roll = rand() % (max - min + 1) + min;
+    return roll; // return roll to get_input function
 }
 
 void get_input() // get input function uses no parameters and returns void
 {
+    srand(time(0)); // include random numbers
+    auto die_roll = 0; // initialize die roll as 0
+    auto choice = 'y';
 
+    do // run this at least once then prompt user for input on whether or not to continue
+    {
+        for(int i=0; i<11; i++) // loop iterates 10 times and calls roll_die fn
+        {   
+            die_roll = roll_die();
+            std::cout<<die_roll<<"\n";
+        }
+        std::cout<<"Would you like to roll the dice again?\nEnter y to roll again, enter any other key to exit...\n";
+        std::cin>>choice;
+    } while(choice == 'Y' || choice == 'y'); // continue rolling dice 10 times as long as user wishes to continue
 }
--- test/question_test_3/question_tests_3.cpp
+++ test/question_test_3/question_tests_3.cpp
@@ -2,11 +2,18 @@
 #include "catch.hpp"
 #include "question3.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
 }
 
 TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
+}
+
+TEST_CASE("Verify that roll die function returns a random number between 1 and 6")
+{
+	REQUIRE(roll_die() > 0 && roll_die() < 7); // require roll die function to produce result between 1 and 6
+											   // function iterates 10 times using for loop within a do/while loop
 }
