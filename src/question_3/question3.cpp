08ed02f127a003e9fda9507992076f26aff2369f
cole-fiedler <123602079+cole-fiedler@users.noreply.github.com>
2023-03-09 16:56:39 +0000

    question 3 updates

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -22,15 +22,12 @@ void get_input() // get input function uses no parameters and returns void
 {
     srand(time(0)); // include random numbers
     auto die_roll = 0; // initialize die roll as 0
-    auto choice = 'y';
+    auto choice = 'y'; // initialize choice as y
 
     do // run this at least once then prompt user for input on whether or not to continue
-    {
-        for(int i=0; i<11; i++) // loop iterates 10 times and calls roll_die fn
-        {   
-            die_roll = roll_die();
-            std::cout<<die_roll<<"\t";
-        }
+    {  
+        die_roll = roll_die();
+        std::cout<<die_roll<<"\t";
         std::cout<<"\n\nWould you like to roll the dice again?\nEnter y to roll again, enter any other key to exit...\t";
         std::cin>>choice;
     } while(choice == 'Y' || choice == 'y'); // continue rolling dice 10 times as long as user wishes to continue
